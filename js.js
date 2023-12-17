3230321437d8cf38c9e2c4501a82bdb3116123b6
Yogesh Yadav <113727273+yogeshyadav999@users.noreply.github.com>
2023-12-17 15:02:28 +0530

    Add files via upload

--- js.js
+++ js.js
@@ -70,7 +70,7 @@ function deleteDigit()
         var act=0, b=0;
         act=document.getElementById("text-box").value;
         b=act.charAt(act.length-1);
-        if(b=='+' || b=='-' || b=='*' || b=='/')
+        if(b=='+' || b=='-' || b=='*' || b=='/' || b=='%')
         {
             document.getElementById("text-box").value=act.substring(0, act.length-1);
             document.getElementById("text-box").value+="%";
@@ -78,4 +78,20 @@ function deleteDigit()
         else{
             document.getElementById("text-box").value+='%';
         }
+        
+    }
+    function dot()
+    {
+        var act=0, b=0;
+        act=document.getElementById("text-box").value;
+        b=act.charAt(act.length-1);
+        if(b=='.')
+        {
+            document.getElementById("text-box").value=act.substring(0, act.length-1);
+            document.getElementById("text-box").value+=".";
+        }
+        else{
+            document.getElementById("text-box").value+='.';
+        }
+        
     }
